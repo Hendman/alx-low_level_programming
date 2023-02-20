c81ceac63760432d15af123279fd6e667c756f5b
Hendrix Mweema <mweemahendrix8@gmail.com>
2023-02-20 12:20:52 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Aways 0.
  */
 int main(void)
